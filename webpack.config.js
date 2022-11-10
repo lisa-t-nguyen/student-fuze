97f4447220032e5687f309929dc083db95ac804b
Lisa Nguyen <lisanguyensc@gmail.com>
2022-11-10 17:35:33 +0000

    Added more objects to rules array in webpack.config.js for Material Library

--- webpack.config.js
+++ webpack.config.js
@@ -2,6 +2,7 @@ require('dotenv/config');
 const path = require('path');
 const webpack = require('webpack');
 const ReactRefreshWebpackPlugin = require('@pmmmwh/react-refresh-webpack-plugin');
+const autoprefixer = require('autoprefixer');
 
 const clientPath = path.join(__dirname, 'client');
 const serverPublicPath = path.join(__dirname, 'server', 'public');
@@ -57,13 +58,26 @@ module.exports = {
           { loader: 'extract-loader' },
           { loader: 'css-loader' },
           {
+            loader: 'postcss-loader',
+            options: {
+              postcssOptions: {
+                plugins: [
+                  autoprefixer()
+                ]
+              }
+            }
+          },
+          {
             loader: 'sass-loader',
             options: {
               // Prefer Dart Sass
               implementation: require('sass'),
 
               // See https://github.com/webpack-contrib/sass-loader/issues/804
-              webpackImporter: false
+              webpackImporter: false,
+              sassOptions: {
+                includePaths: ['./node_modules']
+              }
             }
           }
         ]
