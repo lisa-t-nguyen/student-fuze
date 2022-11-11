d2a55d6c225a08aee6d5ba64df9412b0980cfa21
Lisa Nguyen <lisanguyensc@gmail.com>
2022-11-11 05:35:41 +0000

    Added navigation menu and search input with header

--- client/app.jsx
+++ client/app.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import Home from './pages/home';
+// import Navigation from './components/navigation';
 
 export default class App extends React.Component {
   render() {
---
+++ client/components/navigation.jsx
@@ -0,0 +1,129 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
+import CssBaseline from '@mui/material/CssBaseline';
+import Drawer from '@mui/material/Drawer';
+import IconButton from '@mui/material/IconButton';
+import List from '@mui/material/List';
+import ListItem from '@mui/material/ListItem';
+import ListItemButton from '@mui/material/ListItemButton';
+import ListItemText from '@mui/material/ListItemText';
+import MenuIcon from '@mui/icons-material/Menu';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
+import TextField from '@mui/material/TextField';
+
+const drawerWidth = 240;
+
+function Navigation(props) {
+  const { window } = props;
+  const [mobileOpen, setMobileOpen] = React.useState(false);
+
+  const handleDrawerToggle = () => {
+    setMobileOpen(!mobileOpen);
+  };
+
+  const drawer = (
+    <div>
+      <Toolbar/>
+      <List>
+        <TextField
+          id="outlined-read-only-input"
+          label="Search"
+          defaultValue="Student name or ID"
+          InputProps={{
+            readOnly: true
+          }}
+          sx={{ fontFamily: 'Poppins', marginLeft: 1 }}
+        />
+        <h1 style={{ fontFamily: 'Poppins', paddingLeft: 12 }}>Navigation Menu</h1>
+        {['Add a Student', 'Attendance', 'Demographics', 'Grades', 'Notes', 'Referrals', 'Supports'].map((text, index) => (
+          <ListItem key={text} disablePadding sx={{ fontFamily: 'Poppins' }}>
+            <ListItemButton>
+              <ListItemText primary={text} sx={{ fontFamily: 'Poppins' }}/>
+            </ListItemButton>
+          </ListItem>
+        ))}
+      </List>
+    </div>
+  );
+
+  const container = window !== undefined ? () => window().document.body : undefined;
+
+  return (
+    <Box sx={{ display: 'flex' }}>
+      <CssBaseline />
+      <AppBar
+        style={{ backgroundColor: '#4B6869' }}
+        position="fixed"
+        sx={{
+          width: { sm: `calc(100% - ${drawerWidth}px)` },
+          ml: { sm: `${drawerWidth}px` }
+        }}
+      >
+        <Toolbar>
+          <IconButton
+            color="inherit"
+            aria-label="open drawer"
+            edge="start"
+            onClick={handleDrawerToggle}
+            sx={{ mr: 2, display: { sm: 'none' } }}
+          >
+            <MenuIcon />
+          </IconButton>
+          <Typography variant="h6" noWrap component="div" style={{ fontFamily: 'Poppins', fontWeight: 'bolder', letterSpacing: 2, fontSize: 30 }}>
+            Student Fuze
+          </Typography>
+        </Toolbar>
+      </AppBar>
+      <Box
+        component="nav"
+        sx={{ width: { sm: drawerWidth }, flexShrink: { sm: 0 } }}
+        aria-label="mailbox folders"
+      >
+        {/* The implementation can be swapped with js to avoid SEO duplication of links. */}
+        <Drawer
+          container={container}
+          variant="temporary"
+          open={mobileOpen}
+          onClose={handleDrawerToggle}
+          ModalProps={{
+            keepMounted: true // Better open performance on mobile.
+          }}
+          sx={{
+            display: { xs: 'block', sm: 'none' },
+            '& .MuiDrawer-paper': { boxSizing: 'border-box', width: drawerWidth }
+          }}
+        >
+          {drawer}
+        </Drawer>
+        <Drawer
+          variant="permanent"
+          sx={{
+            display: { xs: 'none', sm: 'block' },
+            '& .MuiDrawer-paper': { boxSizing: 'border-box', width: drawerWidth }
+          }}
+          open
+        >
+          {drawer}
+        </Drawer>
+      </Box>
+      <Box
+        component="main"
+        sx={{ flexGrow: 1, p: 3, width: { sm: `calc(100% - ${drawerWidth}px)` } }}
+      >
+        <Toolbar />
+      </Box>
+    </Box>
+  );
+}
+Navigation.propTypes = {
+  /**
+   * Injected by the documentation to work in an iframe.
+   * You won't need it on your project.
+   */
+  window: PropTypes.func
+};
+
+export default Navigation;
